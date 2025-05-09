c56b0b0b6e8d02b172afcf6930ec3689c8efabe7
SABARINATH-23 <sabarinath0211@gmail.com>
2025-05-10 01:18:49 +0530

    business completed

--- News/src/Pages/Business.jsx
+++ News/src/Pages/Business.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 const Business = ()=>{
+
+    let[state,setState] = useState([]);
+    useEffect(()=>{
+        data();
+    },[])
+
+    let data = async ()=>{
+        try{
+            let obj = await axios.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e5143dbe6aa04574aa8f068cfbba41ac");
+            let finalData = obj.data.articles;
+            setState((state)=>([
+                ...state,...finalData
+            ]));
+            
+        }catch(e)
+        {
+            console.log(e)
+        }
+    }
     return(
         <>
-            <h1>Business</h1>
+            {
+                state.map((ele,index)=>{
+                    return(
+
+                        ele.urlToImage != null &&(
+                        <div key={index}>
+                            <p>{ele.title}</p>
+                            <p>{ele.author}</p>
+                            <img src={ele.urlToImage} alt="ele.image_url"/>
+                        </div>
+                        )
+                    )
+                })
+            }
         </>
     )
 }
